551742c495519e6428123b691a86bf2346c1a036
JeongGil Lee <nainom2002@gmail.com>
2025-12-29 10:10:59 +0900

    .

---
+++ DirectX/Bin/EngineAsset/Shader/GlobalShader2D.hlsl
@@ -0,0 +1,88 @@
+#include "Share.hlsli"
+
+struct VS_INPUT_COLOR2D
+{
+	// POSITION = POSITION0
+	float3 Pos : POSITION0;
+	float4 Color : COLOR0;
+};
+
+struct VS_OUTPUT_COLOR2D
+{
+	// Registers prefixed with SV_ are used exclusively for passing values in the Vertex Shader (VS). 
+	// They must not be used elsewhere.
+
+	// system value position
+	float4 Pos : SV_POSITION;
+	float4 Color : COLOR0;
+};
+
+// Texture. use register 0.
+Texture2D tbBaseTexture : register(t0);
+
+// Set entry point name in file option.
+VS_OUTPUT_COLOR2D Color2DVS(VS_INPUT_COLOR2D input)
+{
+	VS_OUTPUT_COLOR2D output = (VS_OUTPUT_COLOR2D) 0;
+
+	output.Pos = mul(float4(input.Pos, 1.f), cbWVP);
+	output.Color = input.Color;
+
+	return output;
+}
+
+struct PS_OUTPUT_COLOR
+{
+	// print color to 0th back buffer
+	float4 Color : SV_TARGET;
+};
+
+PS_OUTPUT_COLOR Color2DPS(VS_OUTPUT_COLOR2D input)
+{
+	PS_OUTPUT_COLOR output = (PS_OUTPUT_COLOR) 0;
+	output.Color = input.Color;
+	return output;
+}
+
+PS_OUTPUT_COLOR MaterialColor2DPS(VS_OUTPUT_COLOR2D input)
+{
+	PS_OUTPUT_COLOR output = (PS_OUTPUT_COLOR) 0;
+	output.Color = cbBaseColor;
+	output.Color.a = cbOpacity;
+
+	return output;
+}
+
+struct VS_INPUT_TEX
+{
+	float3 Pos : POSITION;
+	float2 UV : TEXCOORD;
+};
+
+struct VS_OUTPUT_TEX
+{
+	float4 Pos : SV_POSITION;
+	float2 UV : TEXCOORD;
+};
+
+VS_OUTPUT_TEX DefaultTexVS(VS_INPUT_TEX input)
+{
+	VS_OUTPUT_TEX output = (VS_OUTPUT_TEX) 0;
+
+	output.Pos = mul(float4(input.Pos, 1.f), cbWVP);
+	output.UV = input.UV;
+
+	return output;
+}
+
+PS_OUTPUT_COLOR MaterialTexPS(VS_OUTPUT_TEX input)
+{
+	PS_OUTPUT_COLOR output = (PS_OUTPUT_COLOR) 0;
+
+	float4 TextureColor = tbBaseTexture.Sample(sbPoint, input.UV);
+
+	output.Color.rgb = TextureColor.rgb * cbBaseColor.rgb;
+	output.Color.a = TextureColor.a * cbOpacity;
+
+	return output;
+}
---
+++ DirectX/Bin/EngineAsset/Shader/Share.hlsli
@@ -0,0 +1,29 @@
+// Write content commonly used in Shader
+
+/*
+ * Register of shader:
+ * Input register: Used to pass vertex information. Used as a combination of NameNumber.
+ * Output register: Used to pass completed vertex information. DirectX uses this information to determine the position on the screen and prepare for color output.
+ * Constant register: Used to pass general data from C++ code to the shader.
+ * Temporary register: A register used during computations.
+ */
+
+// A feature that transfers frequently changing values from the CPU to the GPU.
+cbuffer CBTransform : register(b0)
+{
+	matrix cbWorld;
+	matrix sbView;
+	matrix cbProj;
+	matrix cbWV;
+	matrix cbWVP;
+}
+
+cbuffer CBMaterial : register(b1)
+{
+	float4 cbBaseColor;
+	float cbOpacity;
+	float3 cbMaterialEmpty;
+}
+
+SamplerState sbPoint : register(s0);
+SamplerState sbLinear : register(s1);
